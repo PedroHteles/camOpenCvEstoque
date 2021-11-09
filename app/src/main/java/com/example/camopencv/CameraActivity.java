eb0101878f2c30c3335b50788a61b717917c75c4
PedroHteles <74112798+PedroHteles@users.noreply.github.com>
2021-11-09 16:59:31 -0300

    v2 09011

--- app/src/main/java/com/example/camopencv/CameraActivity.java
+++ app/src/main/java/com/example/camopencv/CameraActivity.java
@@ -1,9 +1,6 @@
 package com.example.camopencv;
 
 
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
@@ -13,60 +10,55 @@ import android.util.Log;
 import android.view.SurfaceView;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.DecodeHintType;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.RGBLuminanceSource;
+import com.google.zxing.Result;
+import com.google.zxing.ResultPoint;
+import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.multi.qrcode.QRCodeMultiReader;
 
 import org.json.JSONObject;
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-
-import com.google.zxing.BinaryBitmap;
-import com.google.zxing.ChecksumException;
-import com.google.zxing.DecodeHintType;
-import com.google.zxing.FormatException;
-import com.google.zxing.LuminanceSource;
-import com.google.zxing.NotFoundException;
-import com.google.zxing.RGBLuminanceSource;
-import com.google.zxing.Reader;
-import com.google.zxing.Result;
-import com.google.zxing.ResultPoint;
-import com.google.zxing.common.HybridBinarizer;
-import com.google.zxing.multi.qrcode.QRCodeMultiReader;
-
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.imgproc.Imgproc;
-import org.opencv.objdetect.QRCodeDetector;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
 
-public class CameraActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2{
-    private static final String TAG="MainActivity";
+public class CameraActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
+    private static final String TAG = "MainActivity";
     private Mat mRgba;
     private Mat mGray;
-    private CameraBridgeViewBase mOpenCvCameraView;
     private Integer flag = 0;
-    private BaseLoaderCallback mLoaderCallback =new BaseLoaderCallback(this) {
+    JavaCameraView javaCameraView;
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
         public void onManagerConnected(int status) {
-            switch (status){
+            switch (status) {
                 case LoaderCallbackInterface
-                        .SUCCESS:{
-                    Log.i(TAG,"OpenCv Is loaded");
-                    mOpenCvCameraView.enableView();
+                        .SUCCESS: {
+                    Log.i(TAG, "OpenCv Is loaded");
+                    javaCameraView.enableView();
                 }
-                default:
-                {
+                default: {
                     super.onManagerConnected(status);
                 }
                 break;
@@ -74,76 +66,79 @@ public class CameraActivity extends Activity implements CameraBridgeViewBase.CvC
         }
     };
 
-    public CameraActivity(){
-        Log.i(TAG,"Instantiated new "+this.getClass());
+    public CameraActivity() {
+        Log.i(TAG, "Instantiated new " + this.getClass());
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        int MY_PERMISSIONS_REQUEST_CAMERA=0;
+        int MY_PERMISSIONS_REQUEST_CAMERA = 0;
         // if camera permission is not given it will ask for it on device
         if (ContextCompat.checkSelfPermission(CameraActivity.this, Manifest.permission.CAMERA)
-                == PackageManager.PERMISSION_DENIED){
-            ActivityCompat.requestPermissions(CameraActivity.this, new String[] {Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
+                == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(CameraActivity.this, new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
         }
         setContentView(R.layout.activity_camera);
-        mOpenCvCameraView=(CameraBridgeViewBase) findViewById(R.id.frame_Surface);
-        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
-        mOpenCvCameraView.setCvCameraViewListener(this);
+        javaCameraView = (JavaCameraView) findViewById(R.id.frame_Surface);
+        javaCameraView.setCameraIndex(0);
+        javaCameraView.setVisibility(SurfaceView.VISIBLE);
+        javaCameraView.setCvCameraViewListener(CameraActivity.this);
+        javaCameraView.enableFpsMeter();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (OpenCVLoader.initDebug()){
+        if (OpenCVLoader.initDebug()) {
             //if load success
-            Log.d(TAG,"Opencv initialization is done");
+            Log.d(TAG, "Opencv initialization is done");
             mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
-        }
-        else{
+        } else {
             //if not loaded
-            Log.d(TAG,"Opencv is not loaded. try again");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_4_0,this,mLoaderCallback);
+            Log.d(TAG, "Opencv is not loaded. try again");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, mLoaderCallback);
         }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if (mOpenCvCameraView !=null){
-            mOpenCvCameraView.disableView();
+        if (javaCameraView != null) {
+            javaCameraView.disableView();
         }
     }
 
-    public void onDestroy(){
+    public void onDestroy() {
         super.onDestroy();
-        if(mOpenCvCameraView !=null){
-            mOpenCvCameraView.disableView();
+        if (javaCameraView != null) {
+            javaCameraView.disableView();
         }
     }
 
-    public void onCameraViewStarted(int width ,int height){
-        mRgba=new Mat(height,width, CvType.CV_8UC4);
-        mGray =new Mat(height,width,CvType.CV_8UC1);
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        mGray = new Mat(height, width, CvType.CV_8UC1);
     }
 
-    public void onCameraViewStopped(){
+    public void onCameraViewStopped() {
         mRgba.release();
     }
 
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame){
-        mRgba=inputFrame.rgba();
-        mGray=inputFrame.gray();
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        mGray = inputFrame.gray();
 
-        if(flag == 0){
-            Bitmap bMap = Bitmap.createBitmap(mRgba.width(), mRgba.height(), Bitmap.Config.ARGB_8888);
-            Utils.matToBitmap(mRgba,bMap);
-            int[] intArray = new int[bMap.getWidth()*bMap.getHeight()];
+
+        if (flag == 0) {
+            Bitmap bMap = Bitmap.createBitmap(mRgba.width(), mRgba.height(), Bitmap.Config.ARGB_4444);
+            Utils.matToBitmap(mRgba, bMap);
+            int[] intArray = new int[bMap.getWidth() * bMap.getHeight()];
             bMap.getPixels(intArray, 0, bMap.getWidth(), 0, 0, bMap.getWidth(), bMap.getHeight());
-            LuminanceSource source = new RGBLuminanceSource(bMap.getWidth(), bMap.getHeight(),intArray);
+            LuminanceSource source = new RGBLuminanceSource(bMap.getWidth(), bMap.getHeight(), intArray);
             BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
             List<Qr> listaProduto = new ArrayList<>();
             List<Qr> listaEndereco = new ArrayList<>();
@@ -152,13 +147,12 @@ public class CameraActivity extends Activity implements CameraBridgeViewBase.CvC
             try {
                 Map<DecodeHintType, String> hints = new HashMap<>();
                 hints.put(DecodeHintType.CHARACTER_SET, "utf-8");
-                Result[]  result = new QRCodeMultiReader().decodeMultiple(bitmap, hints);
-                if(result.length > 0){
+                Result[] result = new QRCodeMultiReader().decodeMultiple(bitmap, hints);
+                if (result.length > 0) {
                     flag = 1;
                 }
 
-
-                for(Result kp : result) {
+                for (Result kp : result) {
                     ResultPoint[] points = kp.getResultPoints();
                     Imgproc.putText(mRgba, kp.getText(), new Point(points[1].getX(), points[1].getY() - 50), Core.FONT_HERSHEY_COMPLEX, 1.0, new Scalar(0, 255, 0), 3, Imgproc.LINE_AA, false);
                     Imgproc.rectangle(mRgba, new Point(points[0].getX(), points[0].getY()), new Point(points[2].getX(), points[2].getY()), new Scalar(0, 0, 0, 0), 15);
@@ -173,22 +167,20 @@ public class CameraActivity extends Activity implements CameraBridgeViewBase.CvC
                     }
                 }
 
-
-                for (int i = 0 ;  i < listaProduto.size(); i++) {
+                for (int i = 0; i < listaProduto.size(); i++) {
 
                     Map<String, String> map = new HashMap<>();
                     map.put("endereco", "");
                     map.put("produto", listaProduto.get(i).getResultado());
                     map.put("status", "");
-
                     Double xp = listaProduto.get(i).getX();
                     Double yp = listaProduto.get(i).getY();
                     Double distanciaTemp = null;
                     String enderecoTemp = null;
-                    String  produtoTemp = listaProduto.get(i).getResultado();
-                    int indexTemp = -1;
+                    String produtoTemp = listaProduto.get(i).getResultado();
+                    int indexTemp = 0;
 
-                    for(int j = 0 ;  j < listaEndereco.size(); j++) {
+                    for (int j = 0; j < listaEndereco.size(); j++) {
                         if (j == 0) {
                             Double xe = listaEndereco.get(j).getX();
                             Double ye = listaEndereco.get(j).getY();
@@ -208,13 +200,11 @@ public class CameraActivity extends Activity implements CameraBridgeViewBase.CvC
                         }
                     }
 
-                    if(enderecoTemp != null)
-                    {
-                        if(produtoTemp.contains( enderecoTemp.replaceFirst("QR02","")))
-                        {
-                            map.put("status","ok");
-                        }else{
-                            map.put("status","erro");
+                    if (enderecoTemp != null) {
+                        if (produtoTemp.contains(enderecoTemp.replaceFirst("QR02", ""))) {
+                            map.put("status", "ok");
+                        } else {
+                            map.put("status", "erro");
 
                         }
                         map.put("endereco", enderecoTemp);
@@ -223,8 +213,8 @@ public class CameraActivity extends Activity implements CameraBridgeViewBase.CvC
                         listaFinal.add(jo);
                     }
                 }
-                if(listaEndereco.size() > 0){
-                    for(int j = 0 ;  j < listaEndereco.size(); j++) {
+                if (listaEndereco.size() > 0) {
+                    for (int j = 0; j < listaEndereco.size(); j++) {
                         Map<String, String> map = new HashMap<>();
                         map.put("endereco", listaEndereco.get(j).getResultado());
                         map.put("produto", "");
@@ -232,12 +222,10 @@ public class CameraActivity extends Activity implements CameraBridgeViewBase.CvC
                         JSONObject jo = new JSONObject(map);
                         listaFinal.add(jo);
                     }
-
                 }
                 flag = 0;
                 Log.d(TAG, listaFinal.toString());
-            }
-            catch (NotFoundException e) {
+            } catch (NotFoundException e) {
                 e.printStackTrace();
             }
         }
